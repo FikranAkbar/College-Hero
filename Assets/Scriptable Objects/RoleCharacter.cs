0723d29a066d160d32311095ef1ffa7cdff565ae
Fikran Akbar <fikranakbar756@gmail.com>
2020-04-09 18:31:53 +0700

    Memulai Mockup01

---
+++ Assets/Scriptable Objects/RoleCharacter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Character/Role")]
+public class RoleCharacter : ScriptableObject
+{
+    public string charName;
+    public int money;
+    public float multiplierFactor;
+    public float health;
+    public int healthInc;
+}
