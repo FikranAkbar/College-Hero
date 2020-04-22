72cd33d4469d467c5266d239c54ebf9545e18180
Fikran Akbar <fikranakbar756@gmail.com>
2020-04-22 12:35:34 +0700

    Test tileset

---
+++ Assets/Scripts/Camera/CamFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollow : MonoBehaviour
+{
+    [SerializeField] Transform TargetToFollow;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(
+            Mathf.Clamp(TargetToFollow.position.x, -6.7f, 6.7f),
+            Mathf.Clamp(TargetToFollow.position.y, -5.6f, 5.6f),
+            -10f);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField] float speed;
+    float dirX, dirY;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dirX = Input.GetAxis("Horizontal") * speed;
+        dirY = Input.GetAxis("Vertical") * speed;
+    }
+
+    private void FixedUpdate()
+    {
+        MoveCharacter();
+    }
+
+    void MoveCharacter()
+    {
+        rb.velocity = new Vector3(dirX, dirY);
+    }
+}
