9987cc36e1e3137355ffcc4b4418dece5219b74b
Fikran Akbar <fikranakbar756@gmail.com>
2020-06-09 14:22:17 +0700

    Making Kos and Kamar Kos Area

--- Assets/Scripts/GameManager/ChangeSceneManager.cs
+++ Assets/Scripts/GameManager/ChangeSceneManager.cs
@@ -26,13 +26,25 @@ public class ChangeSceneManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        spawnLocation.Add("default", new Vector2(-6.36f, 5.52f));
-        spawnLocation.Add("1To2", new Vector2(-12.39f, -0.53f));
-        spawnLocation.Add("1To3", new Vector2(-13.68f, 9.47f));
+        // SPAWN POINT: OUTDOOR
+        // Area Kota
         spawnLocation.Add("2To1", new Vector2(11.5f, -0.86f));
-        spawnLocation.Add("2To3", new Vector2(13.94f, 9.47f));
         spawnLocation.Add("3To1", new Vector2(-2.08f, -7.07f));
+        // Area Taman
+        spawnLocation.Add("1To2", new Vector2(-12.39f, -0.53f));
         spawnLocation.Add("3To2", new Vector2(-5.8f, -7.28f));
+        // Area Universitas
+        spawnLocation.Add("1To3", new Vector2(-13.68f, 9.47f));
+        spawnLocation.Add("2To3", new Vector2(13.94f, 9.47f));
+        // Unknown
+        spawnLocation.Add("default", new Vector2(-6.36f, 5.52f));
+
+        // SPAWN POINT: INDOOR
+        // Area Kos
+        spawnLocation.Add("1ToKos", new Vector2(0.42f, -4.61f));
+        spawnLocation.Add("KamarKosToKos", new Vector2(-2.64f, 2.32f));
+        // Area Kamar Kos
+        spawnLocation.Add("KosToKamarKos", new Vector2(1.72f, -2.91f));
     }
 
     // Update is called once per frame
@@ -80,6 +92,38 @@ public class ChangeSceneManager : MonoBehaviour
             {
                 Initiate.Fade("Maps_AreaTaman", Color.black, 2.0f);
                 toPos = "3To2";
+            }
+        }
+    }
+
+    public static void ChangeArea_OutdoorToIndoor(int from, string to)
+    {
+        if(from == 1)
+        {
+            if(to == "Kos")
+            {
+                Initiate.Fade("Maps_Kos", Color.black, 2.0f);
+                toPos = "1ToKos";
+            }
+        }
+    }
+
+    public static void ChangeArea_IndoorToIndoor(string from, string to)
+    {
+        if(from == "Kos")
+        {
+            if(to == "KamarKos")
+            {
+                Initiate.Fade("Maps_KamarKos", Color.black, 2.0f);
+                toPos = "KosToKamarKos";
+            }
+        }
+        else if(from == "KamarKos")
+        {
+            if(to == "Kos")
+            {
+                Initiate.Fade("Maps_Kos", Color.black, 2.0f);
+                toPos = "KamarKosToKos";
             }
         }
     }
--- Assets/Scripts/Player/PlayerChangeScene.cs
+++ Assets/Scripts/Player/PlayerChangeScene.cs
@@ -20,23 +20,39 @@ public class PlayerChangeScene : MonoBehaviour
     {
         if(collision.tag == "1To2")
         {
-            ChangeSceneManager.ChangeArea_OutdoorToOutdoor(1, 2);
-          
-        } else if (collision.tag == "1To3")
+            ChangeSceneManager.ChangeArea_OutdoorToOutdoor(1, 2);  
+        } 
+        else if (collision.tag == "1To3")
         {
             ChangeSceneManager.ChangeArea_OutdoorToOutdoor(1, 3);
-        } else if (collision.tag == "2To1")
+        } 
+        else if (collision.tag == "2To1")
         {
             ChangeSceneManager.ChangeArea_OutdoorToOutdoor(2, 1);
-        } else if (collision.tag == "2To3")
+        } 
+        else if (collision.tag == "2To3")
         {
             ChangeSceneManager.ChangeArea_OutdoorToOutdoor(2, 3);
-        } else if (collision.tag == "3To1")
+        } 
+        else if (collision.tag == "3To1")
         {
             ChangeSceneManager.ChangeArea_OutdoorToOutdoor(3, 1);
-        } else if (collision.tag == "3To2")
+        } 
+        else if (collision.tag == "3To2")
         {
             ChangeSceneManager.ChangeArea_OutdoorToOutdoor(3, 2);
+        }
+        else if (collision.tag == "ToKos")
+        {
+            ChangeSceneManager.ChangeArea_OutdoorToIndoor(1, "Kos");
+        }
+        else if (collision.tag == "KosToKamarKos")
+        {
+            ChangeSceneManager.ChangeArea_IndoorToIndoor("Kos", "KamarKos");
+        }
+        else if (collision.tag == "KamarKosToKos")
+        {
+            ChangeSceneManager.ChangeArea_IndoorToIndoor("KamarKos", "Kos");
         }
     }
 }
