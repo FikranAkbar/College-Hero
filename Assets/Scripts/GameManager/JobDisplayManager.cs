75458ca3b882b7c14516ac1d44e1a4c4c6fbd003
Fikran Akbar <fikranakbar756@gmail.com>
2020-06-12 12:57:34 +0700

    Add JobUI and LectureUI pt1

---
+++ Assets/Scripts/GameManager/JobDisplayManager.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JobDisplayManager : MonoBehaviour
+{
+    
+    [Header("List Job")]
+    public Jobs[] jobs;
+    [Header("UI Panel")]
+    public GameObject JobPanel;
+    [Header("UI Element")]
+    public Text jobName;
+    public Text clothesReq;
+    public Text creativityReq;
+    public Text intelligenceReq;
+    public Text jobDescription;
+    [Header("Job Duration")]
+    public Text jobDuration;
+    
+    int jobIndex = 0;
+    // in hours
+    int duration = 1; 
+
+    // job panel
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ShowJobInfo();
+        jobDuration.text = duration + " hours";
+    }
+
+    private void ShowJobInfo()
+    {
+        jobName.text = jobs[jobIndex].jobName;
+        clothesReq.text = jobs[jobIndex].clothReq;
+        creativityReq.text = jobs[jobIndex].creativReq + "";
+        intelligenceReq.text = jobs[jobIndex].IntelReq + "";
+        jobDescription.text = jobs[jobIndex].jobDescription;
+    }
+
+    public void Job_NavigateRight()
+    {
+        jobIndex++;
+        if(jobIndex > jobs.Length - 1)
+        {
+            jobIndex = 0;
+        }
+    }
+
+    public void Job_NavigateLeft()
+    {
+        jobIndex--;
+        if(jobIndex < 0)
+        {
+            jobIndex = jobs.Length-1;
+        }
+    }
+
+    public void Duration_Minus()
+    {
+        duration--;
+        if (duration == 0)
+        {
+            duration = 1;
+        }
+    }
+
+    public void Duration_Plus()
+    {
+        duration++;
+        if (duration == 10)
+        {
+            duration = 9;
+        }
+    }
+
+    public void ShowJobPanel()
+    {
+        JobPanel.SetActive(true);
+    }
+
+    public void HideJobPanel()
+    {
+        JobPanel.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Scriptable Objects/Jobs.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Job")]
+public class Jobs : ScriptableObject
+{
+    public string jobName;
+    public string clothReq;
+    public int creativReq;
+    public int IntelReq;
+    [TextArea] public string jobDescription;
+}
